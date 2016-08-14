32ef5fe0e0cb9237b424b944007abc3bbe66b154
Joe Duran <joeduran8@gmail.com>
2016-08-13 23:05:34 -0500

    fix typo

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SessionsController < Api::V1::BaseController 
 
 	def create
-		user = User.find_by_email(params[:email])
-		if user && user.authenticate(params[:password])
+		user = User.find_by_email(params[:session][:email])
+		if user && authenticate(params)
 			token = user.generate_bearer_token
 		render json: {bearer_token:token}
 		else
