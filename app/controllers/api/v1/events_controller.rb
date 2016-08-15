46045ba8611196b366fd3915347703d0eed8c11c
Joe Duran <joeduran8@gmail.com>
2016-08-15 00:14:29 -0500

    attempt to test ajax

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::EventsController < Api::V1::BaseController 
 
   def create
-    user = User.find_by!(bearer_token:params[:bearer_token][0])
+    user = User.find_by(bearer_token: params[:bearer_token][0])
     if user
       event = user.events.create!(
         start_time:params[:event][0], 
@@ -10,13 +10,13 @@ class Api::V1::EventsController < Api::V1::BaseController
     end
     survey = params[:survey][0]
     if survey
-      Survey.create!{
-        severity:survey["severity"].to_i,
-        sleep_quality: survey["sleep_quality"][0].to_i,
-        comments:survey["comments"],
-        causation: survey["causation"],
+      Survey.create!(
+        severity: survey[:severity],
+        sleep_quality: survey[:sleep_quality][0],
+        comments: survey[:comments],
+        causation: survey[:causation],
         event_id: event.id
-      }
+      )
     end
   end
 
--- spec/api_events_controller_spec.rb
+++ spec/api_events_controller_spec.rb
@@ -2,7 +2,7 @@
 # RSpec.describe Api::V1::EventsController, type: :controller do 
 #   describe "POST /events" do 
 #     it "creates a new user object " do 
-#       post :create
+#       post :create, params: {"bearer_token"=>['d6teHc/6w+hFbRzKerV2d1ECi/Vn8dFw8O1Ewbw9ylm3DVmCHwgLoWHM2FM2/7aNBdmPsDSMNicZFJfPfkuTDw=='], "event"=>["26636.966675333", "26637.679870417"], "survey"=>[{"severity"=>"5", "sleep_quality"=>["2"], "causation"=>"Fjdsakl", "comments"=>"Fdsafdsa"}]}
 #       expect(response).to have_http_status 200
 #     end
 #   end
