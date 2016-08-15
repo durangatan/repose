46776da3b295320e037dfe7e6b3394a4a479f5d2
Joe Duran <joeduran8@gmail.com>
2016-08-15 01:21:00 -0500

    fix d3 bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require owl.carousel
-//= require d3
 //= require_tree .
--- app/assets/javascripts/user_graphs.js
+++ app/assets/javascripts/user_graphs.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   var startTimes = [];
   $.ajax({
     method: "GET",
