e5202fadcef15d513c064a25f9bc5240e06e46a5
Joe Duran <joeduran8@gmail.com>
2016-08-14 22:03:40 -0500

    add resources for events api endpoint including surveys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814030110) do
+ActiveRecord::Schema.define(version: 20160815020201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,10 @@ ActiveRecord::Schema.define(version: 20160814030110) do
     t.integer  "severity"
     t.text     "comments"
     t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "sleep_quality"
+    t.text     "causation"
   end
 
   create_table "users", force: :cascade do |t|
