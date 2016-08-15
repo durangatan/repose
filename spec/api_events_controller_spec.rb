7cda3401cb3be5e9ee3646089a1e36f4528391ff
Joe Duran <joeduran8@gmail.com>
2016-08-14 22:27:29 -0500

    some small changes

---
+++ spec/api_events_controller_spec.rb
@@ -0,0 +1,9 @@
+# require "rails_helper"
+# RSpec.describe Api::V1::EventsController, type: :controller do 
+#   describe "POST /events" do 
+#     it "creates a new user object " do 
+#       post :create
+#       expect(response).to have_http_status 200
+#     end
+#   end
+# end
