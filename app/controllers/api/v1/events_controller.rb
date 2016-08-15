b6e456f4b866405a0c304c9ecd38d93d60a307a8
Joe Duran <joeduran8@gmail.com>
2016-08-14 23:47:00 -0500

    fix typo

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::EventsController < Api::V1::BaseController
 
   def create
     @user = User.find_by!(bearer_token:params[:bearer_token][0])
-    if user
+    if @user
       event = @user.Event.create!(
         start_time:params[:event][0], 
         end_time:params[:event][1]
