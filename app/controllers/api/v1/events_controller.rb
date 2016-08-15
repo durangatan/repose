200fbbfc3d4cf496de8db96a54117f65f1fdf90a
Joe Duran <joeduran8@gmail.com>
2016-08-14 23:51:32 -0500

    refactor syntax

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,14 +3,14 @@ class Api::V1::EventsController < Api::V1::BaseController
   def create
     user = User.find_by!(bearer_token:params[:bearer_token][0])
     if user
-      event = user.event.create!(
+      event = user.events.create!(
         start_time:params[:event][0], 
         end_time:params[:event][1]
         )
     end
     survey = params[:survey][0]
     if survey
-      event.survey.create!{
+      event.surveys.create!{
         severity:survey[:severity],
         sleep_quality: survey[:sleep_quality],
         comments:survey[:comments],
