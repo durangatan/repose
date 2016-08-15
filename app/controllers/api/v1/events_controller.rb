04caeb823b383ccb4acef5b001332545ee539b84
Joe Duran <joeduran8@gmail.com>
2016-08-14 23:45:40 -0500

    add event creation route

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::EventsController < Api::V1::BaseController 
 
   def create
-    puts params
+    @user = User.find_by!(bearer_token:params[:bearer_token][0])
+    if user
+      event = @user.Event.create!(
+        start_time:params[:event][0], 
+        end_time:params[:event][1]
+        )
+    end
+    survey = params[:survey][0]
+    if survey
+      event.Survey.create!{
+        severity:survey[:severity],
+        sleep_quality: survey[:sleep_quality],
+        comments:survey[:comments],
+        causation: survey[:causation]
+      }
+    end
   end
 
 end
