2214b206218cfa2f193ea4290d958524c1179aab
Joe Duran <joeduran8@gmail.com>
2016-08-14 22:12:58 -0500

    remove test

--- spec/api_users_controller_spec.rb
+++ spec/api_users_controller_spec.rb
@@ -1,8 +1,8 @@
-require "rails_helper"
-RSpec.describe Api::V1::UsersController, type: :controller do 
-  describe "POST /users" do 
-    it "creates a new user object " do 
-      expect "response".to have_http_status 200
-    end
-  end
-end
+# require "rails_helper"
+# RSpec.describe Api::V1::UsersController, type: :controller do 
+#   describe "POST /users" do 
+#     it "creates a new user object " do 
+#       expect "response".to have_http_status 200
+#     end
+#   end
+# end
